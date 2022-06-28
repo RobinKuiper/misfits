0d6ab4b095e74f38011593119baa173ba38bb39f
Robin Kuiper <robingjkuiper@gmail.com>
2022-06-28 09:25:10 +0200

    Loader color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,18 @@ import 'nprogress/nprogress.css'; //styles of nprogress
 import { Router } from 'next/router';
 import { AnimatePresence } from 'framer-motion';
 
+NProgress.configure({
+  template:
+    ' \
+  <div class="bar" role="bar" style="background: #A2821A;"> \
+    <div class="peg" style="box-shadow: 0 0 10px #A2821A, 0 0 5px #A2821A;"></div> \
+  </div> \
+  <div class="spinner" role="spinner"> \
+    <div class="spinner-icon" style="border-top-color: #A2821A; border-left-color: #705a12;"></div> \
+  </div>',
+  showSpinner: true,
+});
+
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
